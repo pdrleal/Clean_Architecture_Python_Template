39572422e0e54507394a0f919e0f7df5e26aea28
pdrleal <pdrlealpereira1@gmail.com>
2024-04-23 22:57:09 +0100

    Forcing to add .env file.

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,15 @@
 import os
-
+import os.path
 import src
 from application.app import create_app
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+if os.path.isfile(".env") is False:
+    raise FileNotFoundError("No .env file found")
+if "FLASK_CONFIG" not in os.environ:
+    raise ValueError("No FLASK_CONFIG environment variable found")
 app = create_app(os.getenv("FLASK_CONFIG"), dependency_container_packages=[src])
 
 if __name__ == '__main__':
